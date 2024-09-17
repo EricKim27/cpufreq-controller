58a14ef7eeb9c7b536add14aa55e0e90999d83e1
EricKim27 <seohyun.kim@outlook.kr>
2024-09-17 21:00:59 +0900

    added code for checking whether the new policy has been applied.

--- freqcon.c
+++ freqcon.c
@@ -13,11 +13,18 @@ void limit_freq(unsigned int max, unsigned int min)
         }
         pr_info("Current policy for CPU %u: min=%u, max=%u\n", cpu, plcy->min, plcy->max);
         pr_info("Dest frequency: max %d, min %d", max, min);
-        if(max != plcy->cpuinfo.max_freq && min != plcy->cpuinfo.min_freq) {
+        if(max != plcy->cpuinfo.max_freq || min != plcy->cpuinfo.min_freq) {
             plcy->max = (unsigned int)max;
             plcy->min = (unsigned int)min;
             cpufreq_update_policy(cpu);
         }
+        cpufreq_cpu_put(plcy);
+        plcy = cpufreq_cpu_get(cpu);
+        if (!plcy) {
+            pr_err("Failed to re-fetch CPU policy for CPU %u\n", cpu);
+            return;
+        }
+            pr_info("Updated policy for CPU %u: min=%u, max=%u\n", cpu, plcy->min, plcy->max);
         cpufreq_cpu_put(plcy);
     }
 }
