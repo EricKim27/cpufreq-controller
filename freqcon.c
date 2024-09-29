e77f6332a3f3bbff1bbd354847539df4a11e8a52
EricKim27 <seohyun.kim@outlook.kr>
2024-09-29 18:52:17 +0900

    Changed the code to make it work, max frequency needs working.

--- freqcon.c
+++ freqcon.c
@@ -11,20 +11,23 @@ void limit_freq(unsigned int max, unsigned int min)
             pr_err("Failed to get CPU policy for CPU %u\n", cpu);
             return;
         }
+        
         pr_info("Current policy for CPU %u: min=%u, max=%u\n", cpu, plcy->min, plcy->max);
         pr_info("Dest frequency: max %d, min %d", max, min);
         if(max != plcy->cpuinfo.max_freq || min != plcy->cpuinfo.min_freq) {
-            plcy->max = (unsigned int)max;
-            plcy->min = (unsigned int)min;
+            plcy->cpuinfo.max_freq = max;
+            plcy->cpuinfo.min_freq = min;
             cpufreq_update_policy(cpu);
-        }
-        cpufreq_cpu_put(plcy);
-        plcy = cpufreq_cpu_get(cpu);
-        if (!plcy) {
-            pr_err("Failed to re-fetch CPU policy for CPU %u\n", cpu);
-            return;
-        }
+            cpufreq_cpu_put(plcy);
+            
+            plcy = cpufreq_cpu_get(cpu);
+            if (!plcy) {
+                pr_err("Failed to re-fetch CPU policy for CPU %u\n", cpu);
+                return;
+            }
             pr_info("Updated policy for CPU %u: min=%u, max=%u\n", cpu, plcy->min, plcy->max);
-        cpufreq_cpu_put(plcy);
+        } else {
+            cpufreq_cpu_put(plcy);
+        }
     }
 }
